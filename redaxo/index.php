44fda79bfed2e9edc07f1f1ef3f1c55820602444
Markus Staab <markus.staab@redaxo.de>
2012-06-28 15:32:07 +0200

    WIP xhprof plugin

--- redaxo/index.php
+++ redaxo/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// TODO enable/disable using GUI
+require 'src/addons/debug/plugins/xhprof/vendor/xhprof/external/header.php';
+
 $REX = array();
 $REX['REDAXO'] = true;
 $REX['HTDOCS_PATH'] = '../';
@@ -8,3 +11,6 @@ $REX['BACKEND_FOLDER'] = 'redaxo';
 require 'src/core/master.inc.php';
 
 require rex_path::core('index_be.inc.php');
+
+// TODO check why the shutdown func is not run to include the footer
+require 'src/addons/debug/plugins/xhprof/vendor/xhprof/external/footer.php';
